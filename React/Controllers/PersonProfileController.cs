ec779fbf76321ff69976ae2c3fa2b1200ba5e64b
Raminta Stebulytė <papugiuke@gmail.com>
2018-06-28 15:27:35 +0300

    End of day

---
+++ React/Controllers/PersonProfileController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProfileController : Controller
+    {
+        public List<Profile> Get()
+        {
+            return ProfileDataBase.Profiles;
+        }
+
+
+        [HttpGet("{id}")]
+        public Profile Get(int id)
+        {
+            return ProfileDataBase.Profiles.Single(x => x.Id == id);
+        }
+
+        [HttpDelete("{id}")]
+        public bool Delete(int id)
+        {
+            var victim = ProfileDataBase.Profiles.Single(x => x.Id == id);
+            return ProfileDataBase.Profiles.Remove(victim);
+        }
+        [HttpPost]
+        public bool Post([FromBody] Profile profile)
+        {
+            ProfileDataBase.Profiles.Add(profile);
+            return true;
+        }
+    }
+}
---
+++ React/Models/ProfileDataBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class ProfileDataBase
+    {
+        public static List<Profile> Profiles = new List<Profile>(
+            new[]
+            {
+                new Profile { Id = 1, Name = "Tomas", Age = 17 },
+                new Profile { Id = 2, Name = "Joana", Age = 18 }
+            }
+        );
+        public static bool Add(int id, string name, int age)
+        {
+            if (id > Profiles.Count())
+            {
+                Profiles.Add(new Profile() { Id = id, Name = name, Age = age });
+                return true;
+            }
+            return false;
+        }
+    }
+}
